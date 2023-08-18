f8ce501d62619929f46e0c7afa89dba20056481e
Nguyễn Hữu Tứ <huutu0427@gmail.com>
2023-08-18 14:44:46 +0700

    update 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,19 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.safetynet.SafetyNet
 import com.google.android.gms.safetynet.SafetyNetApi
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 
-class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks {
-//class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var btnClick: Button
     private lateinit var checkBox: CheckBox
     private val siteKey = "6LdjOrYnAAAAAFEcmN8YGsGhJxeGy2zufBZXhjnZ"
-    private lateinit var googleApiClient: GoogleApiClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,14 +22,6 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks {
         checkBox = findViewById(R.id.checkbox)
         btnClick = findViewById(R.id.btnClick)
 
-        googleApiClient =
-            GoogleApiClient.Builder(this).addApi(SafetyNet.API).addConnectionCallbacks(this).build()
-
-//        checkBox.setOnClickListener {
-//            if (checkBox.isChecked) {
-//                SafetyNet.SafetyNetApi.verifyWithRecaptcha(googleApiClient, siteKey).setResu
-//            }
-//        }
         checkBox.setOnClickListener {
             if (checkBox.isChecked) {
                 SafetyNet.getClient(this).verifyWithRecaptcha(siteKey)
@@ -57,29 +46,25 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks {
             }
         }
 
-//        btnClick.setOnClickListener {
-//            SafetyNet.getClient(this).verifyWithRecaptcha(siteKey)
-//                .addOnSuccessListener { response ->
-//                    if (response.tokenResult?.isNotBlank() == true) {
-//                        // Captcha verification successful, proceed with your logic
-//                        checkBox.text = "Captcha verification successful, proceed with your logic"
-//                    } else {
-//                        // Captcha verification failed
-//                        checkBox.text = "Captcha verification failed"
-//
-//                    }
-//                }
-//                .addOnFailureListener { e ->
-//                    // Error occurred during captcha verification
-//                    checkBox.text = "Error occurred during captcha verification"
-//
-//                }
-//        }
+        btnClick.setOnClickListener {
+            SafetyNet.getClient(this).verifyWithRecaptcha(siteKey)
+                .addOnSuccessListener { response ->
+                    if (response.tokenResult?.isNotBlank() == true) {
+                        // Captcha verification successful, proceed with your logic
+                        checkBox.text =
+                            "Captcha verification successful, proceed with your logic ${response.tokenResult}"
+                    } else {
+                        // Captcha verification failed
+                        checkBox.text = "Captcha verification failed"
+
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Error occurred during captcha verification
+                    checkBox.text = "Error occurred during captcha verification $e"
+
+                }
+        }
     }
 
-    override fun onConnected(p0: Bundle?) {
-    }
-
-    override fun onConnectionSuspended(p0: Int) {
-    }
 }
